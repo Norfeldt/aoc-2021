d0573d9818990ccd05ce1c3b5cedfc20078551d2
Lasse Norfeldt <lasse@norfeldt.com>
2021-12-05 22:00:15 +0100

    add template to speed up things

---
+++ template.ts
@@ -0,0 +1,16 @@
+import { describe, expect, it, run } from 'https://deno.land/x/tincan/mod.ts'
+import { log, logResult } from '../helpers.ts'
+
+describe('XX of december - ', () => {
+  const exampleData = []
+
+  describe('part 1', () => {
+    it('example for part 1', () => {})
+  })
+
+  describe('part 2', () => {
+    it('example for part 1', () => {})
+  })
+})
+
+run()
