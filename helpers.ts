5ff7d14c0ce2a68c98fc0089e3ee7b2694ed0da9
Lasse Norfeldt <lasse@norfeldt.com>
2021-12-09 19:27:41 +0100

    make log helper accept `any` type

--- helpers.ts
+++ helpers.ts
@@ -1,4 +1,4 @@
 import { bgBrightYellow, black } from 'https://deno.land/std/fmt/colors.ts'
 
-export const log = (mgs: string | number) => console.log(bgBrightYellow(black(` ${mgs} `)))
+export const log = (mgs: any) => console.log(bgBrightYellow(black(` ${mgs} `)))
 export const logResult = (result: string | number) => log(`result: ${result}`)
